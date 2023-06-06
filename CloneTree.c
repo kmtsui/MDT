98d8c8c2e34b196207e75fb8e33043c97ba09f71
kmtsui <a122014@hotmail.com>
2023-06-06 11:25:04 +0900

    Add explanation

--- CloneTree.c
+++ CloneTree.c
@@ -3,13 +3,14 @@ void CloneTree(std::string wcsimfile, std::string mdtfile)
     TFile oldfile(wcsimfile.c_str());
     
     TFile newfile(mdtfile.c_str(),"update");
-    for (auto treename : {"wcsimGeoT", "wcsimRootOptionsT", "fRooTrackerOutputTree"})
+    for (auto treename : {/*"Settings",*/ "wcsimGeoT", "wcsimRootOptionsT", "fRooTrackerOutputTree"}) // Settings tree is corrupted
     {
         TTree* t = (TTree*)oldfile.Get(treename);
         TTree* nt = t->CloneTree();
         newfile.cd();
         nt->Write();
     }
+
     newfile.Close();
     oldfile.Close();
 }
