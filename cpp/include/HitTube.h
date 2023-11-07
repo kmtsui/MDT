622553b7dca4f308a07ddcf465cfc440520a7816
kmtsui <a122014@hotmail.com>
2023-11-07 16:45:44 +0900

    Save mPMT id in HitTube class

--- cpp/include/HitTube.h
+++ cpp/include/HitTube.h
@@ -29,6 +29,10 @@ class HitTube
         virtual ~HitTube();
         void SetTubeID(const int i){ fTubeID = i; }
         int GetTubeID() const { return fTubeID; }
+        void SetmPMTID(const int i){ fmPMTID = i; }
+        int GetmPMTID() const { return fmPMTID; } 
+        void SetmPMT_PMTID(const int i){ fmPMT_PMTID = i; }
+        int GetmPMT_PMTID() const { return fmPMT_PMTID; } 
 		float GetPosition(int i) const { return fPosition[i]; }
 		float GetOrientation(int i) const { return fOrientation[i]; }
 		void SetPosition(int i, float f) { fPosition[i] = f; }
@@ -76,6 +80,8 @@ class HitTube
         vector<TrueHit*> fPhotoElectrons;
 
         int fTubeID;
+        int fmPMTID;
+        int fmPMT_PMTID;
         int fNDigiHits;
 		float fPosition[3];
 		float fOrientation[3];
